ac1b634394852c2bb10809a4a11d1ce0ced82954
aatmanrajyaguru <aatmanrajyaguru@gmail.com>
2023-12-15 00:35:57 -0500

    JS Code for login page

---
+++ js/login.js
@@ -0,0 +1,20 @@
+const switchers = [...document.querySelectorAll('.switcher')]
+var login = document.getElementById("login-email").value;
+var password = document.getElementById("login-password").value;
+switchers.forEach(item => {
+	item.addEventListener('click', function() {
+		switchers.forEach(item => item.parentElement.classList.remove('is-active'))
+		this.parentElement.classList.add('is-active')
+	})
+})
+function myFunction(event){
+    event.preventDefault();
+    login = document.getElementById("login-email").value;
+    password = document.getElementById("login-password").value;
+    if(login === 'aatman@gmail.com' && password === 'hello123'){
+        window.location = "/solesphere.html"  
+    }
+    else{
+        // Code for wrong email password
+    }
+}
